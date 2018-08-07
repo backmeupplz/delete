b613bb6f1de17772fa697d294aef5bba37829b63
backmeupplz <backmeupplz@gmail.com>
2018-08-06 23:02:30 -0700

    finish up the skeleton for facebook app

--- facebook.js
+++ facebook.js
@@ -4,11 +4,12 @@ Vue.component('facebook', {
       wallPosts: true,
       photos: true,
       showExtra: false,
+      token: '',
     }
   },
   computed: {
     deletable: function() {
-      return this.wallPosts || this.photos
+      return this.token && (this.wallPosts || this.photos)
     }
   },
   methods: {
@@ -18,7 +19,10 @@ Vue.component('facebook', {
   },
   template: `
   <div>
-    <p>So you decided to clean up your facebook account. Please, select what would you like to remove.</p>
+    <p>So you decided to clean up your facebook account. Please, enter your access token that is fetched from <a href="https://token.delete.live">here</a> and select what would you like to remove.</p>
+    <div class="form-group row">
+      <input type="text" class="form-control" placeholder="Facebook token" v-model="token">
+    </div>
     <div class="form-check">
       <label class="form-check-label">
         <input class="form-check-input" type="checkbox" v-model="wallPosts">
@@ -32,32 +36,10 @@ Vue.component('facebook', {
       </label>
     </div>
     <div class="text-center">
-      <p v-if="!deletable">Make sure you select at least one thing to delete!</p>
       <button class="btn btn-primary" @click.prevent='deleteFacebook()' :disabled="!deletable">Delete!</button>
     </div>
     <br>
-
-    <div v-if="!showExtra">
-      <p>Don't trust this website? You shouldn't! Would you like to enter your own Facebook API keys?</p>
-      <button class="btn btn-secondary" @click.prevent='showExtra = true'>Show API key setup</button>
-    </div>
-    <div v-if="showExtra">
-      <p>Just enter your Facebook API keys below and hit the <code>Delete</code> button above. You can learn how to obtain your own Facebook app API key and secret <a href="https://google.com">here</a>.</p>
-      <form>
-        <div class="form-group row">
-          <label for="apiKey" class="col-sm-2 col-form-label">API key</label>
-          <div class="col-sm-10">
-            <input type="text" class="form-control" id="apikey" placeholder="API key">
-          </div>
-        </div>
-        <div class="form-group row">
-          <label for="secret" class="col-sm-2 col-form-label">Secret</label>
-          <div class="col-sm-10">
-            <input type="text" class="form-control" id="secret" placeholder="Secret">
-          </div>
-        </div>
-      </form>
-    </div>
+    <p v-if="!deletable">Make sure you select at least one thing to delete and provide the access token</p>
   </div>
   `,
 })
