8901b7c82505ab9ab71f9fef234f23d73b837d7c
backmeupplz <backmeupplz@gmail.com>
2018-08-08 22:06:26 -0700

    finish experiment

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 [![Delete.live](/docs/img/logo.png?raw=true)](http://voicybot.com/)
 
+> Attention! Facebook doesn't allow apps to delete content that hasn't been created by this app. Unfortunately, the idea of simple posts deletion is impossible to implement at the moment of writing this message. Maybe at some point in future Facebook will ease their limits and this website will be up. Cheers!
+
 ## [Delete.live](https://delete.live) source code
 The code of the website that allows you to remove data from Facebook with a press of a button. Hosted with [GitHub Pages](https://pages.github.com/).
 
--- docs/index.js
+++ docs/index.js
@@ -1,27 +1,26 @@
 var main = new Vue({
   el: '#main',
   data: {
-    wallPosts: true,
     photos: true,
     likes: true,
     videos: true,
+    logStrings: [],
   },
   methods: {
-    deleteFacebook(token) {
-      alert('To be developed!')
-    }
+    deleteFacebook() {
+      if (!this.deletable) {
+        alert('Please, select at least one thing to delete')
+        return
+      }
+      deleteData(this.photos, this.likes, this.videos)
+    },
   },
   computed: {
-    deletable: function() {
-      return this.wallPosts || this.photos || this.likes || this.videos
+    log: function() {
+      return this.logStrings.join('')
     },
-    permissions: function() {
-      const list = []
-      if (this.wallPosts) list.push('user_posts')
-      if (this.photos) list.push('user_photos')
-      if (this.likes) list.push('user_likes')
-      if (this.videos) list.push('user_videos')
-      return list.join(',')
+    deletable: function() {
+      return this.photos || this.likes || this.videos
     },
   }
 })
@@ -56,4 +55,134 @@ function handleFacebook(response) {
     return
   }
   main.deleteFacebook(response.authResponse.accessToken)
+}
+
+function deleteData(photos, likes, videos) {
+  const thingsToDelete = []
+  if (photos) thingsToDelete.push('photos')
+  if (likes) thingsToDelete.push('likes')
+  if (videos) thingsToDelete.push('videos')
+  append(`🔥 You have selected to delete: ${thingsToDelete.join(', ')}`)
+  append('🚀 Fetching data from Facebook...')
+  let p = getMe()
+  if (likes) {
+    p = p.then(() => deleteLikes())
+  }
+  p.then(() => {
+    append(`🎉 Great! All the data you requested has been deleted!`)
+  })
+}
+
+function append(str) {
+  main.logStrings.push(str)
+}
+
+function getMe() {
+  return new Promise((res) => {
+    FB.api(
+      '/me',
+      'GET',
+      {},
+      function(response) {
+        append(`🦄 Fetched your name (${response.name}) and Facebook ID (${response.id})`)
+        res()
+      }
+    );
+  })
+}
+
+function deleteLikes() {
+  return getLikes()
+    .then((posts) => {
+      append(`👍 Found ${posts.length} likes, deleting...`)
+      return deleteObjects(posts)
+    })
+}
+
+function getLikes() {
+  return getObjects('/me/likes')
+}
+
+function deleteObjects(objects) {
+  return new Promise((res) => {
+    append(`🔥 Deleting ${objects.length} objects...`)
+    const promises = []
+    for (const object of objects) {
+      promises.push(deleteObject(object))
+    }
+    Promise.all(promises)
+      .then(() => {
+        res()
+      })
+      .catch((err) => {
+        append(`❗️ Error: ${err.message}`)
+        res()
+      })
+  })
+}
+
+function getObjects(endpoint) {
+  let acc = []
+  return new Promise((res) => {
+    FB.api(
+      endpoint,
+      'GET',
+      {},
+      function(response) {
+        acc = acc.concat(response.data.map(v => v.id))
+        const previous = response.paging.previous
+        const next = response.paging.next
+        getObjectsAtUrl(previous)
+          .then((objects) => {
+            acc = acc.concat(objects)
+            return getObjectsAtUrl(next)
+          })
+          .then((objects) => {
+            acc = acc.concat(objects)
+            res(acc)
+          })
+      }
+    );
+  })
+}
+
+function deleteObject(id) {
+  return new Promise((res) => {
+    FB.api(
+      `/${id}`,
+      'DELETE',
+      {},
+      function(response) {
+        if (response.error) {
+          append(`❗️ Error deleting objects: ${response.error.message}`)
+        }
+        res()
+      }
+    );
+  })
+}
+
+function getObjectsAtUrl(url) {
+  return new Promise((res) => {
+    if (!url || !url.length) return res([])
+    let acc = []
+    axios.get(url)
+      .then((response) => {
+        acc = acc.concat(response.data.data)
+        const paging = response.data.paging || {previous: '', next: ''}
+        return getObjectsAtUrl(paging.previous)
+          .then((iObjects) => {
+            acc = acc.concat(iObjects)
+            return getObjectsAtUrl(paging.next)
+          })
+          .then((iObjects) => {
+            acc = acc.concat(iObjects)
+            res(acc)
+          })
+      })
+      .catch((err) => {
+        append(`❗️ Error getting objects: ${err.message}`)
+        res(acc)
+      })
+  })
 }
